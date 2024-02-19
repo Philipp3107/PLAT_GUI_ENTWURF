186805756ccf0153567c36b88a4420fa9394278b
Philipp Kotte <philippkotte@icloud.com>
2024-02-19 23:04:14 +0100

    chore: refactor Analyse class in client/cmd/login.go
    The Analyse class in the login.go file has been refactored to improve code readability and maintainability. 
    
    Changes include:
    - Consolidated duplicate variable declarations and removed unnecessary comments.
    - Added missing comments to improve code documentation.
    - Sorted the keys in the DataStorage before adding them as branches to the FlowPane.
    - Added a heading label for the view.
    
    These changes were made to enhance the structure and organization of the code, making it easier to understand and maintain.

--- src/main/java/org/flimwip/design/Views/Analyse.java
+++ src/main/java/org/flimwip/design/Views/Analyse.java
@@ -26,63 +26,83 @@ public class Analyse extends VBox {
     private final MainController controller;
 
     /**
-     *
+     * DataStorage for the {@link Checkout Checkouts}
      */
     private final DataStorage ds;
 
+    /**
+     * Main FlowPain for displaying all Branches
+     */
     private FlowPane main;
+
+    /**
+     * The String with which the User is currently filtering the Branches
+     */
     private String search = "";
+
+    /**
+     * ScrollPane for displaying the FlowPane {@link Analyse#main}
+     */
     private ScrollPane sp;
+
+    /**
+     * Container for {@link Analyse#main}
+     */
     private HBox m;
+
+    /**
+     * Constructor
+     * @param controller providing basic functionality
+     * @param ds DataStorage for the {@link Checkout Checkouts}
+     */
     public Analyse(MainController controller, DataStorage ds) {
         this.ds = ds;
         this.controller = controller;
 
+        // Favorites currently not in use
         HBox fav = new HBox();
         FlowPane favorites = new FlowPane(5, 10);
         favorites.setOrientation(Orientation.HORIZONTAL);
         favorites.setMaxHeight(100);
+
+        //Build up of m
         this.m = new HBox();
         m.setPrefWidth(1400);
+        HBox.setHgrow(m, Priority.ALWAYS);
+        VBox.setVgrow(m, Priority.ALWAYS);
+
+        //setup of main
         this.main = new FlowPane(5, 5);
         this.main.setPrefWrapLength(1261);
         this.main.setOrientation(Orientation.HORIZONTAL);
 
-        Set<String> sets = ds.list_keys();
-        List<String> list = new ArrayList<>(sets.stream().toList());
+        //Sorting of the Keys in the DataStorage
+        List<String> list = new ArrayList<>(ds.list_keys().stream().toList());
         Collections.sort(list);
 
+        //Adding the Keys as Branches to the FlowPane
         for(String s: list){
             Branch nl = new Branch(s, ds.get_nl_name(s), ds.get_nl_region(s), false, this);
             main.getChildren().add(nl);
         }
+
         fav.getChildren().add(favorites);
+
         m.getChildren().add(main);
         this.sp = new ScrollPane(m);
         sp.setFitToWidth(true);
-        //sp.setPadding(new Insets(10));
         sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         VBox.setVgrow(sp, Priority.ALWAYS);
         HBox.setHgrow(sp, Priority.ALWAYS);
         sp.setStyle("-fx-background: #6c708c; -fx-border-color: #6c708c");
 
+        //Heading for this View
         Label heading = new Label("Niederlassungen");
         heading.setStyle("-fx-font-weight: bold; -fx-font-family: 'Fira Mono'; -fx-font-size: 30; -fx-text-fill: white");
 
-        TextField searching = new TextField();
-        searching.setOnKeyPressed(keyEvent -> {
-            if(keyEvent.getCode().toString().equals("BACK_SPACE")){
-                if(!search.isEmpty()){
-                    search = search.substring(0, search.length()-1);
+        TextField searching = serach_text_field();
 
-                }
-            }else{
-                search += keyEvent.getText();
-            }
-
-            filter_center(search);
-
-        });
         HBox search = new HBox(searching);
         search.setAlignment(Pos.CENTER_RIGHT);
         HBox.setHgrow(search, Priority.ALWAYS);
@@ -103,6 +123,24 @@ public class Analyse extends VBox {
 
     }
 
+    /**
+     * Build the Seatch Text Field
+     * @return TextField
+     */
+    private TextField serach_text_field(){
+        TextField searching = new TextField();
+        searching.setOnKeyPressed(keyEvent -> {
+            search = searching.getText();
+            filter_center(search);
+
+        });
+        return searching;
+    }
+
+    /**
+     * Filters the {@link Analyse#sp MainScollPane} based on the Text
+     * @param text to Filter with
+     */
     public void filter_center(String text){
         System.out.println(text);
         this.m.getChildren().remove(this.main);
