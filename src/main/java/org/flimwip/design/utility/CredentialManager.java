e37a0aa26eba61295fb427a0f3e9e3db3471b58b
Philipp Kotte <philippkotte@icloud.com>
2024-02-19 22:36:09 +0100

    fix: refactor file reading and writing logic in CredentialManager class
    - Replaced the usage of `getResourceAsStream` with `FileReader` to read the credentials file.
    - Simplified the logic for reading and writing the credentials file.
    - Added error handling for file operations.
    
    The changes were made to improve the readability and maintainability of the code in the CredentialManager class.

--- src/main/java/org/flimwip/design/utility/CredentialManager.java
+++ src/main/java/org/flimwip/design/utility/CredentialManager.java
@@ -16,37 +16,68 @@ public class CredentialManager {
 
         InputStream stream = CredentialManager.class.getClassLoader().getResourceAsStream("Credentials.csv");
 
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
-            String line = "";
-            while((line = br.readLine()) != null){
-                String[] splitted = line.split(";");
-                this.username = splitted[0];
-                this.password = splitted[1];
-            }
-        }catch(FileNotFoundException e){
-            System.out.println("Couldn't find credentials " );
-        }catch(IOException e){
-            System.out.println("An Error occured." + e.getLocalizedMessage());
+        try(BufferedReader br = new BufferedReader(new FileReader(CREDENTIALS))){
+            password = br.readLine().split(";")[1];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public String get_password(){
-        return this.password;
+    /**
+     * Fetches the Username from the Credentials File and provieds it statically
+     * @return String: Username either "" or provided by user
+     */
+    public static String get_username(){
+        String username = null;
+        try {
+            check_files();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(CREDENTIALS))){
+            username = br.readLine().split(";")[0];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return username;
+
+    }
+        return password;
+
+
+
+    /**
+     * Checks if the File with the Credentials Exists and if not creates it
+     * @throws IOException
+     */
+    private static void check_files() throws IOException {
+        File cred = new File(CREDENTIALS);
+        File dir = new File(DIR);
+        if(!cred.exists()){
+            if(!dir.exists()){
+                dir.mkdirs();
+                cred.createNewFile();
+            }else{
+                cred.createNewFile();
+            }
+        }
     }
 
-
-    public String get_username(){
-    return this.username;
-    }
-
-    public void set_new_credentials(String username, String password){
-    try(BufferedWriter bw = new BufferedWriter(new FileWriter(CREDENTIALS))){
+    /**
+     * Sets a new Password and Username if the User enters it
+     * @param username Username for the application and for fetching Data from the Checkout
+     * @param password Password for the application and for fetching Data from the Checkout
+     * @throws IOException
+     */
+    public void set_new_credentials(String username, String password) throws IOException {
+        File f = new File(CREDENTIALS);
+    try(BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
         bw.write(username + ";" + password);
     } catch (IOException e) {
         throw new RuntimeException(e);
     }
 
-    init();
     }
 
 
