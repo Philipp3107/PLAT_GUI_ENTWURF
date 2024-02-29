57d2df7e7a933499f8a7c866d57725bb99dcab0f
Philipp3107 <philippkotte@icloud.com>
2024-02-29 10:28:48 +0100

    feat: add XML_Vendor_Parser utility class for reading and writing configuration files
    The XML_Vendor_Parser class is added to the org.flimwip.design.utility package. It includes the following constants:
    - CONFIG
    - FROM
    - ABSOLUT
    - TO
    - PATH
    
    The class also includes the following methods:
    - read_config(): This method is used to read the configuration.
    - write_config(): This method is used to write the configuration.

---
+++ src/main/java/org/flimwip/design/utility/XML_Vendor_Parser.java
@@ -0,0 +1,21 @@
+package org.flimwip.design.utility;
+
+public class XML_Vendor_Parser {
+
+    static final String CONFIG = "config";
+    static final String FROM = "from";
+    static final String ABSOLUT = "absolut";
+    static final String TO = "to";
+    static final String PATH = "path";
+
+
+
+    public void read_confif(){
+
+    }
+
+
+    public void write_config(){
+
+    }
+}
