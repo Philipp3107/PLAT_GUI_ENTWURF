397ee3ab9ca22debf965afa8aabd4792ae17a192
PhilippWork <philipp.kotte@gmx.de>
2024-03-13 17:18:11 +0100

    Testing Annotations

--- src/main/java/org/flimwip/design/Sampleclass.java
+++ src/main/java/org/flimwip/design/Sampleclass.java
@@ -6,7 +6,7 @@ import org.flimwip.design.Documentationhandler.ServiceM;
 
 public class Sampleclass {
 
-    @ServiceATT(desc = "name: String")
+    @ServiceATT(desc = "name: String", type = "String")
     private String name;
 
     @ServiceCR(desc = "Constructor of the Class SampleClass", params = {"name : String"})
@@ -14,12 +14,12 @@ public class Sampleclass {
         this.name = name;
     }
 
-    @ServiceM(desc = "Getter method for name", params = {}, returns = {"String - name : Name der Smapleclass"}, thrown = {})
+    @ServiceM(desc = "Getter method for name", category = "", params = {}, returns = "String - name : Name der Smapleclass", thrown = {})
     public String getName() {
         return name;
     }
 
-    @ServiceM(desc = "Setter method for name", params = {"name: String"}, returns = {}, thrown = {})
+    @ServiceM(desc = "Setter method for name", category = "", params = {"name: String"}, returns = "", thrown = {})
     public void setName(String name) {
         this.name = name;
     }
