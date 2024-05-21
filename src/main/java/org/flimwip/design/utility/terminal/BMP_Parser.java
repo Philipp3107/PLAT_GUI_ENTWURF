520325946307011ccd53e27f750c977a89d96131
Philipp3107 <philippkotte@icloud.com>
2024-05-21 10:02:18 +0200

    local changes after editing

--- src/main/java/org/flimwip/design/utility/terminal/BMP_Parser.java
+++ src/main/java/org/flimwip/design/utility/terminal/BMP_Parser.java
@@ -2,4 +2,24 @@ package org.flimwip.design.utility.terminal;
 
 
 public class BMP_Parser {
+
+
+  // Returns the possible Tags to encode from the given Instruction 
+  void define_handling_tags(String instruction){
+
+  }
+
+  //perses the tlv container
+  void parse_tlv(String line){
+  
+
+
+  }
+
+  //parses given bmp codes in the 
+  void parse_bmp(){
+  
+
+  }
+
 }
